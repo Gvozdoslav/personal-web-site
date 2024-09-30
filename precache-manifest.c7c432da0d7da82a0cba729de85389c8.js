cfe37d6c53ca08c2606696124281f06b9c2aa922
Gvozdoslav <remrostrus2123@yandex.ru>
2024-09-30 23:09:28 +0300

    Updates

---
+++ precache-manifest.c7c432da0d7da82a0cba729de85389c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "460cc3e5d03578b9a03b3d8e24302156",
+    "url": "/personal-web-site/index.html"
+  },
+  {
+    "revision": "c17a170f1e683cd64c8f",
+    "url": "/personal-web-site/static/css/main.7452ef4f.chunk.css"
+  },
+  {
+    "revision": "4924947e9e195595b816",
+    "url": "/personal-web-site/static/js/2.29251fa8.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/personal-web-site/static/js/2.29251fa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c17a170f1e683cd64c8f",
+    "url": "/personal-web-site/static/js/main.280b154d.chunk.js"
+  },
+  {
+    "revision": "f4c2e225163a4c952238",
+    "url": "/personal-web-site/static/js/runtime-main.a08d6283.js"
+  },
+  {
+    "revision": "d1f4b2f9539161a7bd03dda44f608bf6",
+    "url": "/personal-web-site/static/media/avatar.d1f4b2f9.png"
+  }
+]);
